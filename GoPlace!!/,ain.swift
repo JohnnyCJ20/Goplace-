7549be574fb5c6ab03ec71b026caee98ce154756
johnny <johnnyjesuscj@gmail.com>
2021-10-11 13:36:10 -0400

    Initial Commit

---
+++ GoPlace!!/,ain.swift
@@ -0,0 +1,23 @@
+//
+//  ,ain.swift
+//  GoPlace!!
+//
+//  Created by user194452 on 10/11/21.
+//
+
+import UIKit
+
+class _ain: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GoPlace!!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoPlace!!
+//
+//  Created by user194452 on 10/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
